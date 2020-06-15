374d53412f6925871cbb32e74876ef56513a1d74
tlsrhksgh <sin99145@naver.com>
2020-06-15 19:47:58 +0900

    Updates

---
+++ precache-manifest.4332f171a9e52a59f0441d86424d395e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58226b95ded16419f00f022d24f595f3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "10d8b6be1a08f9fc6e80",
+    "url": "/movie_app/static/css/main.78c2f938.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d8b6be1a08f9fc6e80",
+    "url": "/movie_app/static/js/main.e5ed3618.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
